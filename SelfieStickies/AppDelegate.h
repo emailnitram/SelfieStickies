954e0dcc601a40097d96787ad9aad77101dddd75
Martin Henry <emailnitram@gmail.com>
2015-11-02 18:57:44 -0800

    Initial Commit

---
+++ SelfieStickies/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SelfieStickies
+//
+//  Created by Martin Henry on 11/2/15.
+//  Copyright © 2015 Martin Henry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
